9545bc337efa5b0ed95d17a70e9dc74350c2b1cc
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-01-14 12:46:55 -0800

    Complete and test HashSet functions

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-// 1. Start by creating a HashMap class or factory function. It’s up to you which you want to use. Then proceed to create the following methods:
-
-// 2. hash takes a value and produces a hash code with it. We did implement a fairly good hash function in the previous lesson. You are free to use that, or if you wish, you can conduct your own research. Beware, this is a deep deep rabbit hole.
-
-// *Hash maps could accommodate various data types for keys like numbers, strings, objects. But for this project, only handle keys of type strings.
-
-// 3. set takes two arguments, the first is a key and the second is a value that is assigned to this key. If a key already exists, then the old value is overwritten.
-//     Remember to grow your buckets size when it needs to, by calculating if your bucket has reached the load factor.
-
-// 4. get takes one argument as a key and returns the value that is assigned to this key. If key is not found, return null.
-
-// 5. has takes a key as an argument and returns true or false based on whether or not the key is in the hash map.
-
-// 6. remove takes a key as argument and removes it from the hash table.
-
-// 7. length returns the number of stored keys in the hash map.
-
-// 8. clear removes all entries in the hash map.
-
-// 9. keys returns an array containing all the keys inside the hash map.
-
-// 10. values returns an array containing all the values.
-
-// 11. entries returns an array that contains each key, value pair. Example: [[firstKey, firstValue], [secondKey, secondValue]]
-
-// Remember that a hash map does not preserve insertion order when you are retrieving your hash map’s data. It is normal and expected for keys and values to appear out of the order you inserted them in.
-// Extra Credit
-
-// Create a class HashSet that behaves the same as a HashMap but only contains keys with no values.
-
 class MapNode {
   constructor(key, value) {
     this.key = key;
@@ -247,31 +217,6 @@ class HashMap {
   }
 }
 
-let bees = new HashMap();
-
-bees.set(`Key1`, `Value1`);
-bees.set(`Key2`, `Value2`);
-bees.set(`Key3`, `Value3`);
-bees.set(`Key4`, `Value4`);
-bees.set(`Key5`, `Value5`);
-
-// bees.set(`Kevin`, `Whaaaat`);
-bees.set(`Sally`, `Shitfuckery`);
-bees.set(`tacos`, `first tacos`);
-bees.set(`tacos`, `second tacos`);
-
-// console.log(bees.get(`Kevin`));
-// console.log(bees.get(`tacos`));
-console.log(bees.has(`tacos`));
-// bees.remove(`tacos`);
-console.log(bees.has(`tacos`));
-console.log(bees.get(`tacos`));
-console.log(bees.length());
-// bees.clear();
-// console.log(bees.length());
-console.log(bees.entries());
-console.log(bees.keys());
-console.log(bees.values());
 
 class SetNode {
   constructor(key) {
@@ -424,7 +369,41 @@ class HashSet {
     }
     return array;
   }
-
-
-
 }
+
+
+
+// ***********************************************
+// ***********************************************
+
+let bees = new HashMap();
+
+bees.set(`Key1`, `Value1`);
+bees.set(`Key2`, `Value2`);
+bees.set(`Key3`, `Value3`);
+bees.set(`Key4`, `Value4`);
+bees.set(`Key5`, `Value5`);
+bees.set(`Sally`, `Shitfuckery`);
+bees.set(`tacos`, `first tacos`);
+bees.set(`tacos`, `second tacos`);
+
+// console.log(bees.get(`tacos`));
+// console.log(bees.length());
+// bees.clear();
+// console.log(bees.length());
+// console.log(bees.entries());
+// console.log(bees.keys());
+// console.log(bees.values());
+
+
+let birds = new HashSet();
+birds.set(`key1`);
+birds.set(`key2`);
+birds.set(`key3`);
+birds.set(`key4`);
+
+console.log(birds.length());
+// birds.clear()
+console.log(birds.length());
+birds.remove(`key2`)
+console.log(birds.keys())
